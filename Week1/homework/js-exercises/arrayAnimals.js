e07d731c77d077afcb44d5d8c8abd0d968cf4922
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-13 18:43:52 +0200

    Added the fifth and sixth exercises.

---
+++ Week1/homework/js-exercises/arrayAnimals.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*Declare variable and assign to it an empty array. 
+Make sure that the name of the variable indicates it contains more than 1 item. 
+For example items instead of item.*/
+let fruits = [];
+
+// Write a console.log statement that explains in words what you think the value of the array is.
+console.log('This is an empty array at the moment.');
+
+// Write a console.log statement that logs the array.
+console.log(fruits);
+
+/*Create a new variable with an array that has 3 of your favorite animals, 
+each in a different string. Make sure the name of the variables says something about what the variable contains.*/
+let myFavoriteAnimals = ['horse', 'bird', 'cheetah'];
+
+// Write a console.log statement that logs the second array.
+console.log(myFavoriteAnimals);
+
+// Add a statement that adds another string ("Piglet") to the array of animals.
+myFavoriteAnimals.push('piglet');
+
+// Write a console.log statement that logs the second array!
+console.log(myFavoriteAnimals);
