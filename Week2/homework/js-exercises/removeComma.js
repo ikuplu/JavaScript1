00a47c056261fe40a576086a0dc977760fa2c6b8
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-19 11:49:18 +0200

    Added first two of the JavaScript exercises

---
+++ Week2/homework/js-exercises/removeComma.js
@@ -0,0 +1,11 @@
+// Declare a string variable called myString.
+let myString = 'hello,this,is,a,difficult,to,read,sentence';
+
+// Log the length of myString.
+console.log(myString.length);
+
+/* Replace the commas with spaces by using replace method to make it more readable. */
+myString = myString.replace(/,/g, " ");
+
+// After replacing the commas, log myString again to check the last form.
+console.log(myString);
---
+++ Week2/homework/js-exercises/reportEvenOdd.js
@@ -0,0 +1,17 @@
+// Create a for loop, that iterates from 0 to 20.
+for (let i = 0; i <= 20; i++) {
+
+  /* Create a conditional statement that checks if the value of the counter variable is odd or even,
+     log the result to the console. */
+  if (i % 2 === 0) {
+    console.log("The number " + i + " is even!");
+  } else {
+    console.log("The number " + i + " is odd!");
+  }
+}
+
+
+
+
+/* If it's odd, log to the console The number [PUT_NUMBER_HERE] is odd!.
+ If it's even, log to the console The number [PUT_NUMBER_HERE] is even!. */
