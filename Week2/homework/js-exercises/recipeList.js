ebbb8e6e64887d069e21c8acdce42899a641181a
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-19 13:10:31 +0200

    Added the third exercise

---
+++ Week2/homework/js-exercises/recipeList.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Declare a variable that holds an empty object literal (your meal recipe).
+let myRecipe = {};
+
+// Declared another object variable to specify the expected titles to log out to the console in the end.
+let myRecipeKeys = {
+  title: "Meal name",
+  servings: "Serves",
+  ingredients: "Ingredients",
+}
+
+/*Give the empty object 3 properties: a title (string), a servings (number) and 
+an ingredients (array of strings) property.*/
+myRecipe.title = 'Pilav';
+myRecipe.servings = 4;
+myRecipe.ingredients = ['400 grams rice', ' 2 tablespoon butter', ' 3 glass water', ' 1/4 tablespoon salt'];
+
+
+// Log each property out separately, using a loop.
+for (let key in myRecipe) {
+  if (myRecipe.hasOwnProperty(key)) {
+  console.log(myRecipeKeys[key] + ": " +  myRecipe[key]);
+}
+}
