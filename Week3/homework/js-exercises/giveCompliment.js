b5112b6f59ec315fd29c9f5b41be15bc1bc2985f
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-25 13:50:27 +0200

    Added the first JS exercise

---
+++ Week3/homework/js-exercises/giveCompliment.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Write a function with one parameter as a name.
+function giveCompliment(name) {
+  
+  // Define a variable that holds an array with 10 compliments as strings.
+  const compliments = [
+    'great',
+    'amazing',
+    'awesome',
+    'excellent',
+    'wonderful',
+    'the best',
+    'unbelievable',
+    'supersmart',
+    'so cool',
+    'fantastic',
+  ];
+
+  //Write code that randomly selects a compliment.
+  const compliment = compliments[Math.floor(Math.random() * compliments.length)];
+
+  // Return a string: "You are [COMPLIMENT], [YOUR_NAME]!
+  let output = console.log("You are " + compliment + ", " + name + "!");
+  return output;
+}
+
+//Call the function three times, giving each function call the same argument: your name.
+giveCompliment('Ismail');
+giveCompliment('Ismail');
+giveCompliment('Ismail');
