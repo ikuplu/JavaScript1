4b3d4d2abcd08051663be608ae69d3b6c47bfe85
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-26 09:00:02 +0200

    Added the second exercise

---
+++ Week3/homework/js-exercises/calculateDogAge.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Write a function named calculateDogAge.
+function calculateDogAge (age) {
+  
+  //Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.
+  let dogAge = age * 7;
+
+  //Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
+  let output = console.log("Your doggie is " + dogAge + " years old in dog years!");
+  return output;
+}
+
+//Call the function three times with different sets of values.
+calculateDogAge(3);
+calculateDogAge(6);
+calculateDogAge(9);
