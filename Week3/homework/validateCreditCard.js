874b5486e5e4b6a06f5f2e7bc3a8a271352b8a32
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-27 11:23:43 +0200

    Made changes on if statements inside Credit Card Validator project

--- Week3/homework/validateCreditCard.js
+++ Week3/homework/validateCreditCard.js
@@ -3,31 +3,31 @@
 // Write a function called validateCreditNumber.
 function validateCreditNumber(num) {
 
-  let arr = Array.from(num, Number);  // Convert input into an array of numbers.
-  let sum = arr.reduce(function(a, b) { return a + b; }, 0);   // Find the sum of the numbers in the array.
-
   // Input must be 16 characters.
   if (num.length !== 16) {
     return console.log("Invalid! The input " + num + " must contain 16 characters!");
   } 
 
   // All characters must be numbers.
-  else if (num.match(/\D/) !== null) {
+  if (num.match(/\D/) !== null) {
     return console.log("Invalid! The input " + num + " should contain only numbers!");
   }
 
+  let arr = Array.from(num, Number);  // Convert input into an array of numbers.
+  let sum = arr.reduce(function(a, b) { return a + b; }, 0);   // Declare a variable as the sum of the numbers in the array.
+
   // At least two different numbers should be represented.
-  else if ((sum / arr.length) === arr[0]) {
+  if ((sum / arr.length) === arr[0]) {
     return console.log("Invalid! The input " + num + " should contain at least two different numbers!");
   }
 
   // The last number must be even.
-  else if (arr[arr.length-1] % 2 !== 0) {
+  if (arr[arr.length-1] % 2 !== 0) {
     return console.log("Invalid! The input " + num + " should have an even last number!");
   }
 
   // The sum of all the numbers must be greater than 16.
-  else if (sum <= 16) {
+  if (sum <= 16) {
     return console.log("Invalid! The sum of all the numbers of input " + num + " must be greater than 16!");
   }
 
@@ -43,4 +43,3 @@ validateCreditNumber('6666666666666666');
 validateCreditNumber('6666646575843625');
 validateCreditNumber('1111111111111110');
 validateCreditNumber('4657837366577868');
-
